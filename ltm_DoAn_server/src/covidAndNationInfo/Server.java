b7282e5480cb749c005a58d80a8e7203fcfbfa77
hiiieu <hieubochet1@gmail.com>
2021-12-03 13:57:38 +0700

    thread

--- ltm_DoAn_server/src/covidAndNationInfo/Server.java
+++ ltm_DoAn_server/src/covidAndNationInfo/Server.java
@@ -17,11 +17,97 @@ import java.net.Socket;
 public class Server {
 	private Socket socket=null;
 	private ServerSocket server=null;
-	BufferedReader in=null;
-	BufferedWriter out=null;
-	ObjectOutputStream outObj=null;
-	static String clientKey="";
+	//viết xử lý trong đây
+	private class ClientHandler implements Runnable {
+		BufferedReader in=null;
+		BufferedWriter out=null;
+	 	private final Socket socketClient;
+	 	 public ClientHandler(Socket socket)
+	        {
+	            this.socketClient = socket;
+	        }
+	 	public void run(){
+	 		//khi client kết nối hoàn tất
+			System.out.println("client đã kết nối");
+			
+			
+			try {
+				InputStream input= socketClient.getInputStream();
+				OutputStream output = socketClient.getOutputStream();
+				in = new BufferedReader(new InputStreamReader(input));
+				out = new BufferedWriter(new OutputStreamWriter(output));
+				
+				/*Thiết lập mã hóa*/
+				MaHoaDoiXung aes = new MaHoaDoiXung();
+				MaHoaCongKhai rsa = new MaHoaCongKhai();
+				//1. gửi public key tới client
+				String publicKey = MaHoaCongKhai.getPublicKeyString();
+				out.write(publicKey);
+				out.newLine();
+				out.flush();
+				//2. nhận key đối xứng và iv
+				String clientKeyString_encode = in.readLine();
+				String IV = in.readLine();
+				//3. giải mã key để nhận clientKey
+				String clientKeyString=  rsa.decrypt(clientKeyString_encode);
+				String iv = rsa.decrypt(IV);
+				aes.initFromStrings(clientKeyString, iv);
+				System.out.println("thiết lập mã hóa thành công, private key:" + clientKeyString);
+				
+				/*chọn chức năng*/
+				Transport transport = new Transport(aes);//truyền aes để thực hiện mã và giải
+				do {
+						String choose = transport.receive(in);
+						if(choose.equals("Menuclose")) {
+								System.out.println("client đã thoát");
+								break;
+						}
+						if (choose.equals("covid")) {
+								transport.send(out, "bạn đã chọn tra cứu thông tin covid");
+								//viết covid (class covid)
+						}
+						
+						if (choose.equals("nation")) {
+								//transport.send(out, "bạn đã chọn tra cứu thông tin quốc gia");
+								SinhVien sv = new SinhVien("hieu", 10);
+								transport.send(output, sv);
+		//						//quốc gia (class nation)
+		//						//gửi danh sách quốc gia về client để đổ vào combobox
+		//						ListCountry lstCountry = new ListCountry();
+		//						sendToClient(out, lstCountry.getListName());//trả về dãy string ngăn cách bởi dấu ,
+		//						//gửi danh sách thành phố
+		//						/*
+		//								tạo thêm đối tượng city trong country làm típ
+		//						*/
+		//						//idInfo theo quy định: quốc gia = n+stt ,  thành phố = c+stt
+		//						String idInfo = receiveFromClient(in);
+		//						Country country = new Country();
+		//						if (idInfo.matches("^n\\d+$")) {
+		//								//lấy thông tin quốc gia
+		//								int id = Integer.parseInt( idInfo.replace("n", "") );
+		//								country = lstCountry.getCountryById(id);
+		//								//gửi thông tin 
+		//								sendToClient(outObj, country);
+		//						}
+		//						if (idInfo.matches("^c\\d+$")) {
+		//								//lấy thông tin thành phố
+		//								int id = Integer.parseInt( idInfo.replace("c", "") );
+		//								country = lstCountry.getCountryById(id);
+		//								//gửi thông tin 
+		//								//sendToClient(outObj, country.getCityById() );
+		//						}
+								
+						}
+						//Thread.sleep(5000);
+				}while(true);
+				socketClient.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+	 	}
+}
 	
+	//tạo server đợi client
 	public Server(int port) {
 				//tạo server đợi client
 				try {
@@ -35,92 +121,21 @@ public class Server {
 							try {
 								System.out.println("server đang đợi yêu cầu");
 								socket = server.accept();
-								//khi client kết nối hoàn tất
-								System.out.println("client đã kết nối");
-								InputStream input = socket.getInputStream();
-								OutputStream output = socket.getOutputStream();
-								in = new BufferedReader(new InputStreamReader(input));
-								out = new BufferedWriter(new OutputStreamWriter(output));
-								
-								/*Thiết lập mã hóa*/
-								MaHoaDoiXung aes = new MaHoaDoiXung();
-								MaHoaCongKhai rsa = new MaHoaCongKhai();
-								//1. gửi public key tới client
-								String publicKey = MaHoaCongKhai.getPublicKeyString();
-								out.write(publicKey);
-								out.newLine();
-								out.flush();
-								//2. nhận key đối xứng và iv
-								String clientKeyString_encode = in.readLine();
-								String IV = in.readLine();
-								//3. giải mã key để nhận clientKey
-								String clientKeyString=  rsa.decrypt(clientKeyString_encode);
-								String iv = rsa.decrypt(IV);
-								aes.initFromStrings(clientKeyString, iv);
-								
-								
-								/*chọn chức năng*/
-								Transport transport = new Transport(aes);//truyền aes để thực hiện mã và giải
-								do {
-										String choose = transport.receive(in);
-										if(choose.equals("Menuclose")) {
-												System.out.println("client đã thoát");
-												break;
-										}
-										if (choose.equals("covid")) {
-												transport.send(out, "bạn đã chọn tra cứu thông tin covid");
-												//viết covid (class covid)
-										}
-										
-										if (choose.equals("nation")) {
-												//transport.send(out, "bạn đã chọn tra cứu thông tin quốc gia");
-												SinhVien sv = new SinhVien("hieu", 10);
-												transport.send(output, sv);
-						//						//quốc gia (class nation)
-						//						//gửi danh sách quốc gia về client để đổ vào combobox
-						//						ListCountry lstCountry = new ListCountry();
-						//						sendToClient(out, lstCountry.getListName());//trả về dãy string ngăn cách bởi dấu ,
-						//						//gửi danh sách thành phố
-						//						/*
-						//								tạo thêm đối tượng city trong country làm típ
-						//						*/
-						//						//idInfo theo quy định: quốc gia = n+stt ,  thành phố = c+stt
-						//						String idInfo = receiveFromClient(in);
-						//						Country country = new Country();
-						//						if (idInfo.matches("^n\\d+$")) {
-						//								//lấy thông tin quốc gia
-						//								int id = Integer.parseInt( idInfo.replace("n", "") );
-						//								country = lstCountry.getCountryById(id);
-						//								//gửi thông tin 
-						//								sendToClient(outObj, country);
-						//						}
-						//						if (idInfo.matches("^c\\d+$")) {
-						//								//lấy thông tin thành phố
-						//								int id = Integer.parseInt( idInfo.replace("c", "") );
-						//								country = lstCountry.getCountryById(id);
-						//								//gửi thông tin 
-						//								//sendToClient(outObj, country.getCityById() );
-						//						}
-												
-										}
-										//Thread.sleep(5000);
-								}while(true);
-								socket.close();
-	
+								ClientHandler client = new ClientHandler(socket);
+								new Thread(client).start();
 							} catch (IOException e) {
 								System.err.print("client đã ngắt kết nối.");
 							}
 				}
 				//xong hết rồi đóng hết để giải phóng tài nguyên
 				try {
-					in.close();
-					out.close();
 					server.close();
 					System.out.print("server đã tắt");
 				} catch (IOException e) {
 						System.err.print("không đóng được socket,server,inout");
 				}
 	}
+	
 	public static void main(String[] args) {
 		Server ser=new Server(6000);
 	}
