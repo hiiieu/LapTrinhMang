ccfd66853980f125b8bc2625b45f8c989465f983
hiiieu <hieubochet1@gmail.com>
2021-12-23 21:35:43 +0700

    up getjson moi

--- ltm_DoAn_server/src/FetchInfo/ListCity.java
+++ ltm_DoAn_server/src/FetchInfo/ListCity.java
@@ -3,6 +3,7 @@ package FetchInfo;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
@@ -38,7 +39,34 @@ public class ListCity {
 				lstCity.add(ci);
 			}
 	}
-	
+	public String getJsonMoi(String s) {
+		try {
+			HttpURLConnection httpConn = null;
+			URL url = new URL(s);   
+			URLConnection connection = url.openConnection();
+			httpConn = (HttpURLConnection) connection;
+			httpConn.setRequestProperty("Accept", "application/json");
+			httpConn.setRequestMethod("GET");
+			httpConn.setRequestProperty("charset", "utf-8");
+			BufferedReader in;
+			int responseCode = httpConn.getResponseCode();
+			if (responseCode==200) {
+				 in = new BufferedReader(new InputStreamReader(httpConn.getInputStream()));
+			}else
+				 in = new BufferedReader(new InputStreamReader(httpConn.getErrorStream()));
+		    String inputLine;
+		    StringBuffer res=new StringBuffer();
+		    while ( (inputLine = in.readLine()) !=null  ) {
+		    	res.append(inputLine);
+		    }
+		    
+		  in.close();
+		  return (res.toString());
+			} catch (IOException e) {
+				System.err.println(e);
+				return null;
+			}
+	  }
 	public String getJson(String s) {
 		  String sURL = s; //just a string
 
