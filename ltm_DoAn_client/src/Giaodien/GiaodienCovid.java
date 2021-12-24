dccfb369dfbd3054be620ae62bb2ef3a55936682
Khánh Sora <67995752+Elahira@users.noreply.github.com>
2021-12-24 12:48:44 +0700

    update giao dien

--- ltm_DoAn_client/src/Giaodien/GiaodienCovid.java
+++ ltm_DoAn_client/src/Giaodien/GiaodienCovid.java
@@ -76,12 +76,8 @@ public class GiaodienCovid extends JFrame {
 		int f1=0, f2=0;
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");  
 		LocalDateTime now = LocalDateTime.now();  
-<<<<<<< Updated upstream
-		LocalDateTime yesterday = now.minusDays(1);  
-=======
 		LocalDateTime yesterday = now.minusDays(2);
 		now=now.minusDays(1);
->>>>>>> Stashed changes
 		if(txtDate.getText().isEmpty())
 			f1=1;
 		if(txtDate.getText().isEmpty())
@@ -150,28 +146,20 @@ public class GiaodienCovid extends JFrame {
 		
 		JLabel lblNewLabel = new JLabel("Thời gian:");
 		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
-<<<<<<< Updated upstream
 		lblNewLabel.setBounds(235, 133, 68, 17);
-=======
 		lblNewLabel.setBounds(461, 110, 68, 17);
->>>>>>> Stashed changes
 		contentPane.add(lblNewLabel);
 		
 		txtDate = new JTextField();
 		txtDate.setToolTipText("yyyy-mm-dd");
 		txtDate.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		txtDate.setColumns(10);
-<<<<<<< Updated upstream
 		txtDate.setBounds(402, 130, 141, 23);
 		contentPane.add(txtDate);
 		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(375, 88, 168, 23);
-=======
+		comboBox.setBounds(628, 65, 168, 23);
 		txtDate.setBounds(628, 107, 141, 23);
 		contentPane.add(txtDate);
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(601, 65, 168, 23);
->>>>>>> Stashed changes
 		String kq=tranport.receive(in);
 		StringTokenizer tk=new StringTokenizer(kq,";");
 		for(;tk.hasMoreTokens();) {
@@ -209,20 +197,13 @@ public class GiaodienCovid extends JFrame {
 						txtCaNhiem.setText(kqs[1]);
 						txtCaKhoi.setText(kqs[2]);
 						txtTuVong.setText(kqs[3]);
-<<<<<<< Updated upstream
-=======
 						tranport.send(out, "oke");
 						System.out.println(tranport.receive(in));
->>>>>>> Stashed changes
 						String kq2=tranport.receive(in);
 						String[] kq2s = kq2.split(";");
 						txtTotal1.setText(kq2s[0]);
 						txtTotal2.setText(kq2s[1]);
 						txtTotal3.setText(kq2s[2]);
-<<<<<<< Updated upstream
-=======
-						
->>>>>>> Stashed changes
 						DecimalFormat df = new DecimalFormat("0.00");
 						double rate = (1.0)*Integer.parseInt(kq2s[2])/Integer.parseInt(kq2s[0])*100;
 						 
@@ -235,11 +216,6 @@ public class GiaodienCovid extends JFrame {
 						textField_3.setText(kq3s[1]);
 						textField_4.setText(kq3s[3]);
 						
-						
-<<<<<<< Updated upstream
-=======
-						
->>>>>>> Stashed changes
 						//====================================================================
 						Country cts;
 						cts=(Country)tranport.receive(inObj);
@@ -263,7 +239,7 @@ public class GiaodienCovid extends JFrame {
 			}
 		});
 		btnSearch.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		btnSearch.setBounds(801, 66, 89, 45);
+		btnSearch.setBounds(831, 82, 89, 45);
 		contentPane.add(btnSearch);
 		
 		JPanel panel = new JPanel();
@@ -293,62 +269,27 @@ public class GiaodienCovid extends JFrame {
 		panel.add(lblNewLabel_1_3);
 		
 		txtCaKhoi = new JTextField();
-<<<<<<< Updated upstream
-		txtCaKhoi.setBounds(209, 61, 86, 20);
-=======
 		txtCaKhoi.setBounds(209, 61, 171, 20);
->>>>>>> Stashed changes
 		panel.add(txtCaKhoi);
 		txtCaKhoi.setColumns(10);
 		
 		txtTuVong = new JTextField();
 		txtTuVong.setColumns(10);
-<<<<<<< Updated upstream
-		txtTuVong.setBounds(209, 95, 86, 20);
-=======
 		txtTuVong.setBounds(209, 95, 171, 20);
->>>>>>> Stashed changes
 		panel.add(txtTuVong);
 		
 		txtTyLeChet = new JTextField();
 		txtTyLeChet.setColumns(10);
-<<<<<<< Updated upstream
-		txtTyLeChet.setBounds(209, 130, 86, 20);
-=======
 		txtTyLeChet.setBounds(209, 130, 171, 20);
->>>>>>> Stashed changes
 		panel.add(txtTyLeChet);
 		
 		txtCaNhiem = new JTextField();
 		txtCaNhiem.setColumns(10);
-<<<<<<< Updated upstream
 		txtCaNhiem.setBounds(209, 23, 86, 20);
 		panel.add(txtCaNhiem);
 		
 		JLabel lblNewLabel_2 = new JLabel("%");
 		lblNewLabel_2.setBounds(305, 130, 22, 20);
-		panel.add(lblNewLabel_2);
-		
-		JLabel lblNewLabel_3 = new JLabel("Quốc gia có số ca nhiễm cao nhất :");
-		lblNewLabel_3.setBounds(24, 171, 186, 27);
-		panel.add(lblNewLabel_3);
-		
-		JLabel lblNewLabel_4 = new JLabel("Quốc gia có số ca tử vong cao nhất :");
-		lblNewLabel_4.setBounds(24, 209, 186, 27);
-		panel.add(lblNewLabel_4);
-		
-		JLabel lblNewLabel_6 = new JLabel("Số ca");
-		lblNewLabel_6.setBounds(315, 171, 44, 27);
-		panel.add(lblNewLabel_6);
-		
-		JLabel lblNewLabel_6_1 = new JLabel("Số ca");
-		lblNewLabel_6_1.setBounds(315, 209, 44, 27);
-=======
-		txtCaNhiem.setBounds(209, 23, 171, 20);
-		panel.add(txtCaNhiem);
-		
-		JLabel lblNewLabel_2 = new JLabel("%");
-		lblNewLabel_2.setBounds(390, 133, 22, 20);
 		panel.add(lblNewLabel_2);
 		
 		JLabel lblNewLabel_3 = new JLabel("Quốc gia có số ca nhiễm cao nhất :");
@@ -358,145 +299,93 @@ public class GiaodienCovid extends JFrame {
 		
 		JLabel lblNewLabel_4 = new JLabel("Quốc gia có số ca tử vong cao nhất :");
 		lblNewLabel_4.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblNewLabel_4.setBounds(24, 227, 230, 17);
+		lblNewLabel_4.setBounds(11, 228, 230, 17);
 		panel.add(lblNewLabel_4);
 		
-		JLabel lblNewLabel_6 = new JLabel("Số ca :");
+		JLabel lblNewLabel_6 = new JLabel("Số ca:");
 		lblNewLabel_6.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblNewLabel_6.setBounds(198, 202, 43, 17);
+		lblNewLabel_6.setBounds(202, 202, 39, 17);
 		panel.add(lblNewLabel_6);
 		
-		JLabel lblNewLabel_6_1 = new JLabel("Số ca :");
-		lblNewLabel_6_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblNewLabel_6_1.setBounds(198, 255, 43, 17);
->>>>>>> Stashed changes
-		panel.add(lblNewLabel_6_1);
+		JLabel lblNewLabel_6_1 = new JLabel("Số ca");
+		lblNewLabel_6_1.setBounds(315, 209, 44, 27);
+		txtCaNhiem.setBounds(209, 23, 171, 20);
+		panel.add(txtCaNhiem);
 		
 		textField = new JTextField();
 		textField.setColumns(10);
-<<<<<<< Updated upstream
-		textField.setBounds(209, 174, 86, 20);
-=======
 		textField.setBounds(274, 174, 290, 20);
->>>>>>> Stashed changes
 		panel.add(textField);
 		
 		textField_1 = new JTextField();
 		textField_1.setColumns(10);
-<<<<<<< Updated upstream
-		textField_1.setBounds(209, 209, 86, 20);
-=======
 		textField_1.setBounds(274, 228, 290, 20);
->>>>>>> Stashed changes
 		panel.add(textField_1);
 		
 		textField_3 = new JTextField();
 		textField_3.setColumns(10);
-<<<<<<< Updated upstream
-		textField_3.setBounds(359, 174, 86, 20);
-=======
 		textField_3.setBounds(274, 201, 86, 20);
->>>>>>> Stashed changes
 		panel.add(textField_3);
 		
 		textField_4 = new JTextField();
 		textField_4.setColumns(10);
-<<<<<<< Updated upstream
-		textField_4.setBounds(359, 209, 86, 20);
-=======
 		textField_4.setBounds(274, 255, 86, 20);
->>>>>>> Stashed changes
 		panel.add(textField_4);
 		
 		JLabel lblTngSCa = new JLabel("Tổng số ca nhiễm: ");
 		lblTngSCa.setFont(new Font("Tahoma", Font.PLAIN, 14));
-<<<<<<< Updated upstream
-		lblTngSCa.setBounds(314, 26, 118, 17);
-=======
 		lblTngSCa.setBounds(460, 26, 118, 17);
->>>>>>> Stashed changes
 		panel.add(lblTngSCa);
 		
 		JLabel lblTngSCa_1 = new JLabel("Tổng số ca khỏi bệnh: ");
 		lblTngSCa_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
-<<<<<<< Updated upstream
-		lblTngSCa_1.setBounds(314, 64, 141, 17);
-=======
 		lblTngSCa_1.setBounds(460, 64, 141, 17);
->>>>>>> Stashed changes
 		panel.add(lblTngSCa_1);
 		
 		JLabel lblTngSCa_2 = new JLabel("Tổng số ca tử vong :");
 		lblTngSCa_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
-<<<<<<< Updated upstream
-		lblTngSCa_2.setBounds(314, 98, 131, 17);
-=======
 		lblTngSCa_2.setBounds(460, 98, 131, 17);
->>>>>>> Stashed changes
 		panel.add(lblTngSCa_2);
 		
 		txtTotal1 = new JTextField();
 		txtTotal1.setColumns(10);
-<<<<<<< Updated upstream
-		txtTotal1.setBounds(467, 23, 86, 20);
-=======
 		txtTotal1.setBounds(613, 23, 190, 20);
->>>>>>> Stashed changes
 		panel.add(txtTotal1);
 		
 		txtTotal2 = new JTextField();
 		txtTotal2.setColumns(10);
-<<<<<<< Updated upstream
-		txtTotal2.setBounds(467, 61, 86, 20);
-=======
 		txtTotal2.setBounds(613, 61, 190, 20);
->>>>>>> Stashed changes
 		panel.add(txtTotal2);
 		
 		txtTotal3 = new JTextField();
 		txtTotal3.setColumns(10);
-<<<<<<< Updated upstream
-		txtTotal3.setBounds(467, 95, 86, 20);
-=======
 		txtTotal3.setBounds(613, 95, 190, 20);
->>>>>>> Stashed changes
 		panel.add(txtTotal3);
+		
+		JLabel lblNewLabel_6_2 = new JLabel("Số ca:");
+		lblNewLabel_6_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel_6_2.setBounds(202, 256, 39, 17);
+		panel.add(lblNewLabel_6_2);
 		
 		JLabel lblNewLabel_1 = new JLabel("Từ ngày");
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 13));
-<<<<<<< Updated upstream
-		lblNewLabel_1.setBounds(308, 136, 65, 14);
-=======
 		lblNewLabel_1.setBounds(534, 113, 65, 14);
->>>>>>> Stashed changes
 		contentPane.add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_1_1 = new JLabel("Đến ngày");
 		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.PLAIN, 13));
-<<<<<<< Updated upstream
-		lblNewLabel_1_1.setBounds(308, 171, 65, 14);
-=======
 		lblNewLabel_1_1.setBounds(534, 148, 65, 14);
->>>>>>> Stashed changes
 		contentPane.add(lblNewLabel_1_1);
 		
 		txtDate_2 = new JTextField();
 		txtDate_2.setToolTipText("yyyy-mm-dd");
 		txtDate_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		txtDate_2.setColumns(10);
-<<<<<<< Updated upstream
 		txtDate_2.setBounds(402, 169, 141, 23);
-=======
 		txtDate_2.setBounds(628, 146, 141, 23);
->>>>>>> Stashed changes
 		contentPane.add(txtDate_2);
 		
-		
-		
-		
-		
-		
-		
+	
 		
 	}
 }
